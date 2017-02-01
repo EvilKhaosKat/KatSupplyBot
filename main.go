618489ca2a2566e37e5d0a1e5f84962681fddc5d
EvilKhaosKat <evilkhaoskat@gmail.com>
2017-02-01 22:42:21 +0400

    add and list commands

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"os"
 	"bufio"
 	"fmt"
-	telegramBot "gopkg.in/telegram-bot-api.v4"
+	telegramBotApi "gopkg.in/telegram-bot-api.v4"
+	"bytes"
 )
 
 const UPDATES_TIMEOUT = 60
@@ -14,16 +15,21 @@ const COMMAND_ADD = "add"
 const COMMAND_LIST = "list"
 const COMMAND_CLOSE = "close"
 
+var botApi *telegramBotApi.BotAPI
+
+type Request struct {
+	name string
+}
+
+type Bot struct {
+	requests []Request
+	botApi   *telegramBotApi.BotAPI
+}
 
 func main() {
-	log.Println("Trying to read 'token' file")
-	token := readTokenFile()
-	log.Println("Token acquired")
+	bot := getBot()
 
-	bot := getBot(token)
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	for update := range getUpdatesChan(bot) {
+	for update := range bot.getUpdatesChan() {
 		if update.Message == nil {
 			continue
 		}
@@ -33,41 +39,64 @@ func main() {
 }
 
 //Logic of messages handling
-func handleUpdate(update telegramBot.Update, bot *telegramBot.BotAPI) {
+func handleUpdate(update telegramBotApi.Update, bot *Bot) {
 	//log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 	message := update.Message
 
 	if message.IsCommand() {
-		//commandArguments := message.CommandArguments()
+		commandArguments := message.CommandArguments()
 
 		switch command := message.Command(); command {
 		case COMMAND_ADD:
-
+			bot.addRequest(commandArguments)
+			bot.sendReply(update, fmt.Sprintf("Request '%s' added", commandArguments))
 		case COMMAND_LIST:
-
+			bot.sendReply(update, bot.getRequestsText())
 		case COMMAND_CLOSE:
-
+			bot.sendReply(update, "Not implemented yet =)")
 		default:
-			sendReply(update, bot,
-				fmt.Sprintf("I can't understart command '%s'", command))
+			bot.sendReply(update, fmt.Sprintf("I can't understart command '%s'", command))
 		}
 	}
 }
+func (bot *Bot) getRequestsText() string {
+	if len(bot.requests) == 0 {
+		return "No active requests at the moment"
+	}
 
-func sendReply(update telegramBot.Update, bot *telegramBot.BotAPI, text string) {
-	replyMessage := telegramBot.NewMessage(update.Message.Chat.ID, text)
-	replyMessage.ReplyToMessageID = update.Message.MessageID
+	var buffer bytes.Buffer
 
-	bot.Send(replyMessage)
+	for number, request := range bot.requests{
+		buffer.WriteString(fmt.Sprintf("%d: %s\n", number, request.name))
+	}
+
+	return buffer.String()
 }
 
-func getUpdatesChan(bot *telegramBot.BotAPI) <- chan telegramBot.Update {
-	u := telegramBot.NewUpdate(0)
+func getBot() *Bot {
+	log.Println("Trying to read 'token' file")
+	token := readTokenFile()
+	log.Println("Token acquired")
+	botApi = getBotApi(token)
+	log.Printf("Authorized on account %s", botApi.Self.UserName)
+	bot := &Bot{botApi:botApi}
+	return bot
+}
+
+func (bot *Bot) sendReply(update telegramBotApi.Update, text string) {
+	replyMessage := telegramBotApi.NewMessage(update.Message.Chat.ID, text)
+	replyMessage.ReplyToMessageID = update.Message.MessageID
+
+	bot.botApi.Send(replyMessage)
+}
+
+func (bot *Bot) getUpdatesChan() <-chan telegramBotApi.Update {
+	u := telegramBotApi.NewUpdate(0)
 
 	u.Timeout = UPDATES_TIMEOUT
 
-	updates, err := bot.GetUpdatesChan(u)
+	updates, err := bot.botApi.GetUpdatesChan(u)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -75,8 +104,13 @@ func getUpdatesChan(bot *telegramBot.BotAPI) <- chan telegramBot.Update {
 	return updates
 }
 
-func getBot(token string) *telegramBot.BotAPI {
-	bot, err := telegramBot.NewBotAPI(token)
+func (bot *Bot) addRequest(name string) {
+	request := Request{name}
+	bot.requests = append(bot.requests, request)
+}
+
+func getBotApi(token string) *telegramBotApi.BotAPI {
+	bot, err := telegramBotApi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
 	}
